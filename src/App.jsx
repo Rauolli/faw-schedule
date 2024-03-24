44efbb45bd6cb1ea884f312f38aa642e6785cb7b
Rauolli <rauol@t-online.de>
2024-03-24 15:38:57 +0100

    Cange ScheduleTable to start- and endTime, improved testTime, improved displaying Tagesplantabelle

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,17 @@
 import './App.css';
 import DigitalClock from './components/DigitalClock';
 import DateDisplay from './components/DateDisplay';
-// import moment from 'moment';
 import ScheduleTable from './components/ScheduleTable';
 import scheduleData from './data/scheduleData';
 
 
 const App = () => {
-  const currentTime = new Date();
   return (
     <div className="App">
       <DateDisplay />
       <DigitalClock />
       <h1>Tagesplan</h1>
-      <ScheduleTable currentTime={currentTime} scheduleData={scheduleData} />
+      <ScheduleTable scheduleData={scheduleData} />
     </div>
   );
 };
--- src/components/ScheduleTable.jsx
+++ src/components/ScheduleTable.jsx
@@ -1,48 +1,67 @@
 // ScheduleTable.jsx
-
 import React from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import ProgressBar from './ProgressBar';
 
-const ScheduleTable = ({ currentTime, scheduleData }) => {
+const ScheduleTable = ({ scheduleData }) => {
   // current-test-time for development
-  const currentTestTimeString = "14:00";
-  const [hour, minute] = currentTestTimeString.split(':');
+  const testHour = 4;
   const currentTestTime = new Date();
-  currentTestTime.setHours(hour, minute, 0);
+  const minute = currentTestTime.getMinutes();
+  // Beginnend um 8:00 Uhr + der zu bestimmenden testHour
+  currentTestTime.setHours(8 + testHour, minute, 0);
   // for development only
 
+  const [currentTime, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
   const findSelectedRow = () => {
-    const currentHour = currentTime.getHours();
-    const currentMinute = currentTime.getMinutes();
-    
     for (let i = 0; i < scheduleData.length; i++) {
+      const itemStart = new Date();
+      const itemEnd = new Date();
       const item = scheduleData[i];
-      const [hour, minute] = item.time.split(':');
-      const itemTime = new Date();
-      itemTime.setHours(hour, minute, 0);
-      // itemTime.setMinutes(itemTime.getMinutes() - 5);
-      if (itemTime.getHours() === currentHour && itemTime.getMinutes() >= currentMinute){
-        return i;
-      }else if (itemTime.getHours() > currentHour){
+      const [startHour, startMinute] = item.start.split(':');
+      itemStart.setHours(startHour, startMinute, 0);
+      const [endHour, endMinute] = item.end.split(':');
+      itemEnd.setHours(endHour, endMinute, 0);
+
+      if (chooseTestTimeOrCurrentTime() >= itemStart && chooseTestTimeOrCurrentTime() <= itemEnd) {
         return i;
       }
     }
-
-    return -1;
   };
 
   function getProgress (startTime, endTime){ 
-    const [startHour, startMinute] = startTime.split(':');
     const startTimeAsDate = new Date();
+    const endTimeAsDate = new Date();
+    const [startHour, startMinute] = startTime.split(':');
     startTimeAsDate.setHours(startHour, startMinute, 0);
     const [endHour, endMinute] = endTime.split(':');
-    const endTimeAsDate = new Date();
     endTimeAsDate.setHours(endHour, endMinute, 0);
     const wholeTime = endTimeAsDate - startTimeAsDate;
-    const elapsedTime = currentTestTime - startTimeAsDate;
+    const elapsedTime = chooseTestTimeOrCurrentTime() - startTimeAsDate;
 
     return Math.floor((elapsedTime / wholeTime) * 100).toString();
+  }
+
+  function chooseTestTimeOrCurrentTime(){
+    return currentTime <= currentTestTime ? currentTime : currentTestTime;
+  }
+
+  function TimeToString(date){
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    hour < 10 ? '0' + hour : hour;
+    minute < 10 ? '0' + minute : minute;
+    return hour + ':' + minute;
   }
 
   const selectedRow = findSelectedRow();
@@ -50,44 +69,43 @@ const ScheduleTable = ({ currentTime, scheduleData }) => {
     <table>
       <thead>
         <tr>
-          <th>Zeit</th>
+          <th>#</th>
+          <th>von</th>
+          <th>bis</th>
           <th>Aktivität</th>
         </tr>
       </thead>
       <tbody>
         {scheduleData.map((item, index) => {
-          const [hour, minute] = item.time.split(':');
+          const [hour, minute] = item.start.split(':');
           const itemTime = new Date();
           itemTime.setHours(hour, minute, 0);
 
           let rowClassName = '';
           if (selectedRow === index) {
             rowClassName = 'active';
-          } else if (itemTime < currentTime) {
+          } else if (index < selectedRow) {
             rowClassName = 'past';
+          }else{
+            rowClassName = 'future';
           }
 
           return (
             <React.Fragment key={index}>
               <tr className={rowClassName}>
-                <td>{index}</td>
-                <td>{item.time}</td>
+                <td>{index + 1}.</td>
+                <td>{item.start}</td>
+                <td>{item.end}</td>
                 <td>{item.activity}</td>
-                <td>{currentTestTimeString}</td>
               </tr>
-              {index > 0 && (
-              <tr>
-                <td >Start:{scheduleData[index-1]?.time}</td>
-                <td >End:{scheduleData[index]?.time}</td>
-                <td >akt. Zeit:{currentTestTimeString}</td>
-               <td>Progress:{getProgress(scheduleData[index-1]?.time, currentTestTimeString)}</td>
-              </tr>)}
               {selectedRow === index && (
                 <tr className="progress-row">
-                  <td colSpan="4">
-                    <ProgressBar progress={getProgress(scheduleData[index-1]?.time, currentTestTimeString)} />
+                  <td>{TimeToString(chooseTestTimeOrCurrentTime())} Uhr {getProgress(item.start, item.end)}%</td>
+                  <td colSpan="3">
+                    <ProgressBar progress={getProgress(item.start, item.end)} />
                   </td>
                 </tr>
+                
               )}
             </React.Fragment>
           );
