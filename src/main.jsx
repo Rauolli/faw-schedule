5be289b4f8ded67b69ddddc3acc7657b32725e21
Rauolli <rauol@t-online.de>
2024-04-06 17:20:44 +0200

    All CSS-Files referenzed in App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '../src/pages/App.jsx'
-import '../src/styles/base/reset.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,6 +2,8 @@
 
 // import { useState, useEffect } from 'react';
 import '../styles/pages/App.css';
+import '../styles/base/reset.css';
+import '../styles/base/typography.css';
 import DigitalClock from '../components/DigitalClock';
 import DateDisplay from '../components/DateDisplay';
 import ScheduleTable from '../components/ScheduleTable';
